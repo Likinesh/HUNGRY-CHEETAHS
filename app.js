94d65e86e0a5ff07396e7522b7ddd936a49362d7
Likinesh <likithkk2004@gmail.com>
2023-11-17 16:16:29 +0530

    added styles

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const http = require('http');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const authString = btoa(`588795a3-6316-4d77-b30e-e5310f5d2903:41243b5c12f6a06bf92d5848e84c84ae1ff84ce0c9320c14c3841467b0e8454ccb3076aedee6ff70d8fb6e8980d12ab86f53f07b7d0a2715ba1edc69757e42eb82ec3d339358f61b9ee60be87af00b2de635d170e60600118f8935223f2fc1157e6af9d6375a4a806f6554c741ea527f`);
 
 const app = express();
 const port = 3000;
@@ -11,7 +14,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/",(req,res)=>{
-  res.render("index.ejs");
+  res.render("index");
 })
 
 app.get('/iss-location', async (req, res) => {
@@ -26,7 +29,7 @@ app.get('/iss-location', async (req, res) => {
     const result3 = response3.data;
     const response4 = await axios.get('https://api.opencagedata.com/geocode/v1/json?q='+lat+'+'+long+'&key=cb12bd847e3b46a39e84c8d862bbe65a')
     const result4 = response4.data;
-    res.render("iss-location.ejs",{data:result1 , area:result2 , members:result3,place:result4});  
+    res.render("iss-location",{data:result1 , area:result2 , members:result3,place:result4});  
   } catch (error) {
     console.error('Error fetching ISS information:', error.message);
     res.status(500).send('Error fetching ISS information');
@@ -37,7 +40,7 @@ app.get('/NASA-APOD', async (req, res) => {
     try {
       const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`);
       const result = response.data;
-      res.render("NASA-APOD.ejs",{data:result});
+      res.render("NASA-APOD",{data:result});
     } catch (error) {
       console.error('Error fetching APOD information:', error.message);
       res.status(500).send('Error fetching APOD information');
@@ -50,7 +53,7 @@ app.get("/space-news",async(req,res)=>{
     const result1 = response1.data;
     const response2 = await axios.get(`https://api.spaceflightnewsapi.net/v4/reports`);
     const result2 = response2.data;
-    res.render("space-news.ejs",{data1:result1 ,data2:result2});
+    res.render("space-news",{data1:result1 ,data2:result2});
   } catch (error) {
     console.error('Error fetching SpaceFlight API information:', error.message);
     res.status(500).send('Error fetching space news');
@@ -58,14 +61,14 @@ app.get("/space-news",async(req,res)=>{
 });
 
 app.get("/ISRO-info",(req,res)=>{
-    res.render("ISRO_API_data.ejs");
+    res.render("ISRO_API_data");
 })
 
 app.get("/ISRO-customer-satellites",async(req,res)=>{
   try{
     const response1 = await axios.get(`https://isro.vercel.app/api/customer_satellites`);
     const result1 = response1.data;
-    res.render("ISRO-customer-satellites.ejs",{data1:result1});
+    res.render("ISRO-customer-satellites",{data1:result1});
   }
   catch(error){
     console.error('Error fetching ISRO API information:', error.message);
@@ -79,7 +82,7 @@ app.get("/ISRO-data",async(req,res)=>{
     const result1 = response1.data;
     const response2 = await axios.get(`https://isro.vercel.app/api/centres`);
     const result2 = response2.data;
-    res.render("ISRO-Centre.ejs",{data1:result1 ,data2:result2});
+    res.render("ISRO-Centre",{data1:result1 ,data2:result2});
   }
   catch(error){
     console.error('Error fetching ISRO API information:', error.message);
@@ -91,7 +94,7 @@ app.get("/ISRO-stats-l",async(req,res)=>{
   try{
     const response = await axios.get(`https://services.isrostats.in/api/launches`);
     const result = response.data;
-    res.render("ISRO-stats-launches.ejs",{data:result});
+    res.render("ISRO-stats-launches",{data:result});
   }
   catch(error){
     console.error('Error fetching ISRO API information:', error.message);
@@ -103,7 +106,7 @@ app.get("/ISRO-stats-sc",async(req,res)=>{
   try{
     const response = await axios.get(`https://services.isrostats.in/api/spacecraft`);
     const result = response.data;
-    res.render("ISRO-stats-spacecrafts.ejs",{data:result});
+    res.render("ISRO-stats-spacecrafts",{data:result});
   }
   catch(error){
     console.error('Error fetching ISRO API information:', error.message);
@@ -115,7 +118,7 @@ app.get("/next-launch",async(req,res)=>{
   try {
     const response = await axios.request("https://fdo.rocketlaunch.live/json/launches/next/5");
     const result = response.data;
-    res.render("next-launch.ejs",{data:result})
+    res.render("next-launch",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -125,7 +128,7 @@ app.get("/mars-weather",async(req,res)=>{
   try {
     const response = await axios.request("https://api.maas2.apollorion.com");
     const result = response.data;
-    res.render("mars-temp.ejs",{data:result})
+    res.render("mars-temp",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -133,7 +136,7 @@ app.get("/mars-weather",async(req,res)=>{
 
 app.get("/spacex-info",(req,res)=>{
   try {
-    res.render("spacex_API_data.ejs");
+    res.render("spacex_API_data");
   } catch (error) {
     console.error(error);
   }
@@ -143,7 +146,7 @@ app.get("/spacex-starlink",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/starlink");
     const result = response.data;
-    res.render("spacex-starlink.ejs",{data:result})
+    res.render("spacex-starlink",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -153,7 +156,7 @@ app.get("/spacex-history",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/history");
     const result = response.data;
-    res.render("spacex-history.ejs",{data:result})
+    res.render("spacex-history",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -163,7 +166,7 @@ app.get("/spacex-ships",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/ships");
     const result = response.data;
-    res.render("spacex-ships.ejs",{data:result})
+    res.render("spacex-ships",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -173,7 +176,7 @@ app.get("/spacex-crew",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/crew");
     const result = response.data;
-    res.render("spacex-crew.ejs",{data:result})
+    res.render("spacex-crew",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -183,7 +186,7 @@ app.get("/spacex-landpads",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/landpads");
     const result = response.data;
-    res.render("spacex-landpads.ejs",{data:result})
+    res.render("spacex-landpads",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -193,7 +196,7 @@ app.get("/spacex-rockets",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/rockets");
     const result = response.data;
-    res.render("spacex-rockets.ejs",{data:result})
+    res.render("spacex-rockets",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -203,7 +206,7 @@ app.get("/spacex-launchpads",async(req,res)=>{
   try {
     const response = await axios.request("https://api.spacexdata.com/v4/launchpads");
     const result = response.data;
-    res.render("spacex-launchpads.ejs",{data:result})
+    res.render("spacex-launchpads",{data:result})
   } catch (error) {
     console.error(error);
   }
@@ -215,12 +218,12 @@ app.get("/mars-rover",async(req,res)=>{
     if (e_date==='') {
       const response = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&sol=4000");
       const result = response.data;
-      res.render("mars-rover-img.ejs",{data:result});  
+      res.render("mars-rover-img",{data:result});  
     }
     else{
       const response = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&earth_date="+e_date);
       const result = response.data;
-      res.render("mars-rover-img.ejs",{data:result});
+      res.render("mars-rover-img",{data:result});
     }
   } catch (error) {
     console.error(error);
@@ -233,7 +236,7 @@ app.post("/mars-rover-date",(req,res)=>{
 });
 
 app.get("/quiz",(req,res)=>{
-  res.render("quiz.ejs");
+  res.render("quiz");
 })
 
 let planet_name=null;
@@ -241,7 +244,7 @@ app.get("/solar-system",async(req,res)=>{
   if (planet_name===null) {
     const response1 = await axios.request("https://api.le-systeme-solaire.net/rest/knowncount");
     const result1 = response1.data;
-    res.render("solar-system.ejs",{data:result1,solar:null});
+    res.render("solar-system",{data:result1,solar:null});
   }
   try {
     const options = {
@@ -256,7 +259,7 @@ app.get("/solar-system",async(req,res)=>{
     const response2 = await axios.request(options);
     const result2 = response2.data;
     let rad=(result2[0].radius)*69911;
-    res.render("solar-system.ejs",{data:result1 , solar:result2, radi:rad})
+    res.render("solar-system",{data:result1 , solar:result2, radi:rad})
   } catch (error) {
     console.error(error);
   }
@@ -266,7 +269,7 @@ let url = "https://api.spaceflightnewsapi.net/v4/articles";
 app.get("/space-articles",async(req,res)=>{
   const response = await axios.request(url);
   const result =response.data;
-  res.render("space-articles.ejs",{data:result});
+  res.render("space-articles",{data:result});
 });
 
 app.post("/space-articles-prev",async(req,res)=>{
@@ -290,12 +293,16 @@ app.post("/solar-system",(req,res)=>{
     
 })
 
+app.get('/moons',async(req,res)=>{
+  res.render("moon-d")
+})
+
 app.get("/about",(req,res)=>{
-  res.render("about.ejs");
+  res.render("about");
 })
 
 app.get("/contact",(req,res)=>{
-  res.render("contact-us.ejs")
+  res.render("contact-us")
 })
 
 app.listen(port, () => {
